a4a6b61d2081326f12aadf9525e026af1d98db72
movshov <movshov@pdx.edu>
2020-11-26 18:30:47 -0800

    Initial commit with google map api working

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from "react";
+import "./App.css";
+import MyGoogleMap from "./components/MyGoogleMap";
+
+function App() {
+  return (
+    <div className="main-wrapper">
+      <MyGoogleMap />
+    </div>
+  );
+}
+
+export default App;
