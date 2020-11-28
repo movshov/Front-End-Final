25c1c8a79f08bc2e650a7c085d0c4e5dfbb19d68
movshov <movshov@pdx.edu>
2020-11-27 17:56:20 -0800

    Updated project title and added footer with my name

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,12 @@ import React from "react";
 import "./App.css";
 import MyGoogleMap from "./components/MyGoogleMap";
 
+<h1>Corona Restaurant Finder</h1>;
+
+<main>
+  <footer>Created by: Bar Movshovich</footer>
+</main>;
+
 function App() {
   return (
     <div className="main-wrapper">
